623a3b9846d467c02bffb563d87c55a07771e386
javiluli <javiluli.dev@gmail.com>
2026-05-28 18:36:31 +0200

    resolve errores build

--- src/components/content/backgrounds/AnimatedGridBackdrop/index.tsx
+++ src/components/content/backgrounds/AnimatedGridBackdrop/index.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { useEffect, useId, useMemo, useRef, useState } from 'react'
-import { AnimatedGridBackdropProps } from './AnimatedGridBackdrop.types'
+import { AnimatedGridBackdropProps } from './animatedgridbackdrop.types'
 
 interface Square {
   id: number
