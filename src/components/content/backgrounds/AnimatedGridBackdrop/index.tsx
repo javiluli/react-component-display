ada2976b0ffb8b9724ac52545e787d7705e1cf17
javiluli <javiluli.dev@gmail.com>
2026-05-28 18:44:22 +0200

    cambio de nombre

--- src/components/content/backgrounds/AnimatedGridBackdrop/index.tsx
+++ src/components/content/backgrounds/AnimatedGridBackdrop/index.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { useEffect, useId, useMemo, useRef, useState } from 'react'
-import { AnimatedGridBackdropProps } from './animatedgridbackdrop.types'
+import { AnimatedGridBackdropProps } from './types.types'
 
 interface Square {
   id: number
